62c7bc8b675ba79279cf7d1730d88d7aa80437a6
Foxy04321 <foxy04321@gmail.com>
2019-08-27 15:58:23 +0300

    Added app 'Articles'

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     #Local
     'users',
     'pages',
+    'articles',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -112,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'europe/moscow'
 
 USE_I18N = True
 
