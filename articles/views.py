9853dc40b0d1dcb4a8099d1551314968affd3c38
Foxy04321 <foxy04321@gmail.com>
2019-08-29 14:02:22 +0300

    Added mechanics for editing and deleting articles by an authorized user or admin.

--- articles/views.py
+++ articles/views.py
@@ -1,28 +1,56 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 from .models import Article
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import redirect
+from users.models import CustomUser
+
+
 # Create your views here.
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ['title', 'body',]
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+    def form_valid(self, form):
+        user = CustomUser.objects.get(username=self.request.user)
+        if self.request.user != self.object.author and not user.is_superuser:
+            return redirect(reverse('home'))
+        return super().form_valid(form)
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+    def form_valid(self, form):
+        user = CustomUser.objects.get(username=self.request.user)
+        if self.request.user != self.object.author and not user.is_superuser:
+            return redirect(reverse('home'))
+        return super().form_valid(form)
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ['title', 'body', 'author',]
+    fields = ['title', 'body', ]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
